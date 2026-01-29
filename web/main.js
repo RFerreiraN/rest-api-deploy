2b8373e3bfbea276df24482138d777de15b5852e
RFerreiraN <ricardevferreira@gmail.com>
2026-01-29 22:54:20 +0100

    html dinámico

--- web/main.js
+++ web/main.js
@@ -5,14 +5,33 @@ function getProductos() {
       const containArticle = productos.map(producto => {
         return `
         <article data-id="${producto.id}">
-          <h2>${producto.title}</h2>
+          <p>${producto.title}</p>
           <img src="${producto.image}"
           <h3>${producto.price} €</h3>
+          <button>Eliminar</button>
         </article>
         `
       }).join('')
+
       const main = document.getElementById('main')
       main.innerHTML = containArticle
+
+      document.addEventListener('click', (e) => {
+        if (e.target.matches('button')) {
+          const article = e.target.closest('article')
+          const id = article.dataset.id
+
+          fetch(`http://localhost:1314/productos/${id}`, {
+            method: 'DELETE'
+          })
+            .then(res => {
+              if (res.ok) {
+                article.remove()
+              }
+            })
+        }
+      })
     })
 }
+
 getProductos()
